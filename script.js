ae76a06ce5e5abed48a02dbd4536cdd26fc9acbd
Tyler Bowman <114122616+TylerBowman3@users.noreply.github.com>
2024-01-20 14:30:09 -0500

    rewriting functions

--- script.js
+++ script.js
@@ -4,30 +4,28 @@ const suggestions = document.querySelector('.suggestions');
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 function search() {
-	let results = [];
-	let inputVal = input.value;
-	if (inputVal.length) {
-		results = fruit.filter((val) => {
-			return (val.toLowerCase().includes(inputVal.toLowerCase()));
-		});
-		console.log(results);
-	}
-	return results;
+	let inputVal = input.value.toLowerCase();
+	return fruit.filter((val) => val.toLowerCase().includes(inputVal));
 }
 
-function showDropDown(inputVal) {
+function showDropDown() {
+	const inputVal = input.value.trim();
 	const results = search();
-	const ul = document.querySelector('ul');
-	for (let result of results) {
-	if (inputVal !== '') {
-		let li = document.createElement('li');
-		li.innerHTML = '<li>' + "</li>";
-		li.innerText = result;
-		suggestions.appendChild(ul);
-		ul.appendChild(li); 
-		}
-	}
+
+	// clear previous suggestions
+	suggestions.innerHTML = '';
+
+	if (inputVal !== '' && results.length > 0) {
+        const ul = document.createElement('ul');
+        for (let result of results) {
+            let li = document.createElement('li');
+            li.innerText = result;
+            ul.appendChild(li);
+        }
+        suggestions.appendChild(ul);
+    }
 }
 
 
 input.addEventListener('keyup', showDropDown);
+// suggestions.addEventListener('click', useSuggestion);
